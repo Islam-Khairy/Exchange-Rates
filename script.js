26e3f603cd52d4088a6b027be1d7f1b32263a25a
Your Name <you@example.com>
2024-01-18 22:02:14 +0200

    add some modifications to enhance the UX

--- script.js
+++ script.js
@@ -1,12 +1,166 @@
 let convertFrom = document.getElementById("from");
 let convertTo = document.getElementById("to");
 let amountInput = document.getElementById("amount");
-let convertedAmount;
-let exchangeRates; 
+let convertedAmount = 0; 
+let exchangeRates;
 let isFirstTime = true;
-let convertFromCurrency; 
-let convertToCurrency; 
+let convertFromCurrency;
+let convertToCurrency;
 let currencies;
+
+const countryNames = {
+    'AED': 'United Arab Emirates',
+    'AFN': 'Afghanistan',
+    'ALL': 'Albania',
+    'AMD': 'Armenia',
+    'ANG': 'Netherlands Antilles',
+    'AOA': 'Angola',
+    'ARS': 'Argentina',
+    'AUD': 'Australia',
+    'AWG': 'Aruba',
+    'AZN': 'Azerbaijan',
+    'BAM': 'Bosnia and Herzegovina',
+    'BBD': 'Barbados',
+    'BDT': 'Bangladesh',
+    'BGN': 'Bulgaria',
+    'BHD': 'Bahrain',
+    'BIF': 'Burundi',
+    'BMD': 'Bermuda',
+    'BND': 'Brunei',
+    'BOB': 'Bolivia',
+    'BRL': 'Brazil',
+    'BSD': 'Bahamas',
+    'BTN': 'Bhutan',
+    'BWP': 'Botswana',
+    'BZD': 'Belize',
+    'CAD': 'Canada',
+    'CDF': 'Democratic Republic of the Congo',
+    'CHF': 'Switzerland',
+    'CLF': 'Unidad de Fomento (CLF)',
+    'CLP': 'Chile',
+    'CNH': 'China (Offshore)',
+    'CNY': 'China',
+    'COP': 'Colombia',
+    'CUP': 'Cuba',
+    'CVE': 'Cape Verde',
+    'CZK': 'Czech Republic',
+    'DJF': 'Djibouti',
+    'DKK': 'Denmark',
+    'DOP': 'Dominican Republic',
+    'DZD': 'Algeria',
+    'EGP': 'Egypt',
+    'ERN': 'Eritrea',
+    'ETB': 'Ethiopia',
+    'EUR': 'Euro',
+    'FJD': 'Fiji',
+    'FKP': 'Falkland Islands',
+    'GBP': 'United Kingdom',
+    'GEL': 'Georgia',
+    'GHS': 'Ghana',
+    'GIP': 'Gibraltar',
+    'GMD': 'Gambia',
+    'GNF': 'Guinea',
+    'GTQ': 'Guatemala',
+    'GYD': 'Guyana',
+    'HKD': 'Hong Kong',
+    'HNL': 'Honduras',
+    'HRK': 'Croatia',
+    'HTG': 'Haiti',
+    'HUF': 'Hungary',
+    'IDR': 'Indonesia',
+    'ILS': 'Israel',
+    'INR': 'India',
+    'IQD': 'Iraq',
+    'IRR': 'Iran',
+    'ISK': 'Iceland',
+    'JMD': 'Jamaica',
+    'JOD': 'Jordan',
+    'JPY': 'Japan',
+    'KES': 'Kenya',
+    'KGS': 'Kyrgyzstan',
+    'KHR': 'Cambodia',
+    'KMF': 'Comoros',
+    'KPW': 'North Korea',
+    'KRW': 'South Korea',
+    'KWD': 'Kuwait',
+    'KYD': 'Cayman Islands',
+    'KZT': 'Kazakhstan',
+    'LAK': 'Laos',
+    'LBP': 'Lebanon',
+    'LKR': 'Sri Lanka',
+    'LRD': 'Liberia',
+    'LSL': 'Lesotho',
+    'LYD': 'Libya',
+    'MAD': 'Morocco',
+    'MDL': 'Moldova',
+    'MGA': 'Madagascar',
+    'MKD': 'North Macedonia',
+    'MMK': 'Myanmar',
+    'MNT': 'Mongolia',
+    'MOP': 'Macau',
+    'MRU': 'Mauritania',
+    'MUR': 'Mauritius',
+    'MVR': 'Maldives',
+    'MWK': 'Malawi',
+    'MXN': 'Mexico',
+    'MYR': 'Malaysia',
+    'MZN': 'Mozambique',
+    'NAD': 'Namibia',
+    'NGN': 'Nigeria',
+    'NOK': 'Norway',
+    'NPR': 'Nepal',
+    'NZD': 'New Zealand',
+    'OMR': 'Oman',
+    'PAB': 'Panama',
+    'PEN': 'Peru',
+    'PGK': 'Papua New Guinea',
+    'PHP': 'Philippines',
+    'PKR': 'Pakistan',
+    'PLN': 'Poland',
+    'PYG': 'Paraguay',
+    'QAR': 'Qatar',
+    'RON': 'Romania',
+    'RSD': 'Serbia',
+    'RUB': 'Russia',
+    'RWF': 'Rwanda',
+    'SAR': 'Saudi Arabia',
+    'SCR': 'Seychelles',
+    'SDG': 'Sudan',
+    'SEK': 'Sweden',
+    'SGD': 'Singapore',
+    'SHP': 'Saint Helena',
+    'SLL': 'Sierra Leone',
+    'SOS': 'Somalia',
+    'SRD': 'Suriname',
+    'SYP': 'Syria',
+    'SZL': 'Eswatini',
+    'THB': 'Thailand',
+    'TJS': 'Tajikistan',
+    'TMT': 'Turkmenistan',
+    'TND': 'Tunisia',
+    'TOP': 'Tonga',
+    'TRY': 'Turkey',
+    'TTD': 'Trinidad and Tobago',
+    'TWD': 'Taiwan',
+    'TZS': 'Tanzania',
+    'UAH': 'Ukraine',
+    'UGX': 'Uganda',
+    'USD': 'United States',
+    'UYU': 'Uruguay',
+    'UZS': 'Uzbekistan',
+    'VND': 'Vietnam',
+    'VUV': 'Vanuatu',
+    'WST': 'Samoa',
+    'XAF': 'Central African CFA franc',
+    'XCD': 'Eastern Caribbean dollar',
+    'XDR': 'Special Drawing Rights (XDR)',
+    'XOF': 'West African CFA franc',
+    'XPF': 'CFP franc',
+    'YER': 'Yemen',
+    'ZAR': 'South Africa',
+    'ZMW': 'Zambia',
+};
+
 
 function getCurrencies() {
     return fetch(`https://api.fastforex.io/fetch-all?api_key=c08c586352-6a9807a75f-s7a3f5`)
@@ -26,13 +180,17 @@ function getCurrencies() {
 
             for (const currency in currencies) {
                 if (currencies.hasOwnProperty(currency)) {
-                    const option = document.createElement("option");
-                    option.value = currency;
-                    fromDataList.appendChild(option);
-                    toDataList.appendChild(option.cloneNode(true));
+                    const optionFrom = document.createElement("option");
+                    const optionTo = document.createElement("option");
+
+                    const countryName = countryNames[currency] || 'Unknown Country';
+                    optionFrom.value = `${countryName} - ${currency}`;
+                    optionTo.value = `${countryName} - ${currency}`;
+
+                    fromDataList.appendChild(optionFrom);
+                    toDataList.appendChild(optionTo);
                 }
             }
-            
 
             if (!isFirstTime) {
                 convertFrom.value = convertFromCurrency;
@@ -44,10 +202,11 @@ function getCurrencies() {
         });
 }
 
+
 document.getElementById("convert").addEventListener("click", async (event) => {
     await getCurrencies();
-    convertFromCurrency = convertFrom.value;
-    convertToCurrency = convertTo.value;
+    convertFromCurrency = convertFrom.value.split(' - ')[1]; 
+    convertToCurrency = convertTo.value.split(' - ')[1];
 
     if (amountInput.value <= 0 || amountInput.value.trim() === '' || convertFromCurrency === "" || convertToCurrency === "") {
         Swal.fire({
@@ -61,14 +220,19 @@ document.getElementById("convert").addEventListener("click", async (event) => {
     }
 });
 
+
 function getValue(amount, fromExchangeRate, toExchangeRate) {
     if (amount <= 0 || isNaN(amount) || isNaN(fromExchangeRate) || isNaN(toExchangeRate)) {
-        convertedAmount = "";
+        convertedAmount = 0;
     } else {
-        convertedAmount = amount * (toExchangeRate / fromExchangeRate);
+        convertedAmount = (amount / fromExchangeRate) * toExchangeRate;
     }
-    document.getElementById("result").value = convertedAmount.toFixed(2);
+
+    const resultText = `${convertedAmount.toFixed(2)} ${convertToCurrency}`;
+    document.getElementById("result").value = resultText;
 }
+
+
 
 getCurrencies();
 
